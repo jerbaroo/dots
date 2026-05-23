8cfcb5c9fa41e4ec8df76b3b65bb1ba3dfe87473
jerbaroo <jerbaroo.work@pm.me>
2026-05-23 14:49:01 +0200

    Add proton-ge

--- system/steam.nix
+++ system/steam.nix
@@ -1,7 +1,10 @@
+{ pkgs, ... }:
 {
+  programs.gamemode.enable = true;
   programs.steam = {
     dedicatedServer.openFirewall = true;
     enable = true;
+    extraCompatPackages = with pkgs; [ proton-ge-bin ];
     localNetworkGameTransfers.openFirewall = true;
     remotePlay.openFirewall = true;
   };
